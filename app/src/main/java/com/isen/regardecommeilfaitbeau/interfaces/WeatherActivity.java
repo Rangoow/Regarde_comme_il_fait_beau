49b8b54f55d8038eca2e7a7c0be1bbd92f2bbbf8
Noé Esparsa <noe.esp@hotmail.fr>
2020-02-16 22:55:30 +0100

    UI design
    main page designing

--- app/src/main/java/com/isen/regardecommeilfaitbeau/interfaces/WeatherActivity.java
+++ app/src/main/java/com/isen/regardecommeilfaitbeau/interfaces/WeatherActivity.java
@@ -1,8 +1,10 @@
-package com.isen.regardecommeilfaitbeau;
+package com.isen.regardecommeilfaitbeau.interfaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.isen.regardecommeilfaitbeau.R;
 
 public class WeatherActivity extends AppCompatActivity {
 
