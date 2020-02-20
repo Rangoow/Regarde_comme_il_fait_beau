78ff1eae51654a8bdf965740c4e266e3dc28fe68
Langue <louis-clement.langue@isen.yncrea.fr>
2020-02-20 12:05:59 +0100

    make Alert done

--- app/src/main/java/com/isen/regardecommeilfaitbeau/api/meteo/Alert.java
+++ app/src/main/java/com/isen/regardecommeilfaitbeau/api/meteo/Alert.java
@@ -1,13 +1,41 @@
 package com.isen.regardecommeilfaitbeau.api.meteo;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.isen.regardecommeilfaitbeau.typeData.Time;
+
+import org.json.JSONObject;
 
 public class Alert {
     private String title;
     private String severity;
 
-    private Time time;
+    private Time start;
     private Time expires;
+
+    private boolean isMake;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Alert(JSONObject jsonObject, String timeZone){
+        makeObject(jsonObject, timeZone);
+        traductTitle();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void makeObject(JSONObject jsonObject, String timeZone){
+        try{
+            title = jsonObject.getString("title");
+            severity = jsonObject.getString("severity");
+            start = new Time(Integer.toString(jsonObject.getInt("time")), timeZone);
+            expires = new Time(Integer.toString(jsonObject.getInt("expires")), timeZone);
+            isMake = true;
+        }catch (Exception e){
+            isMake = false;
+            e.printStackTrace();
+        }
+    }
 
     private void traductTitle() {
         switch(this.title) {
@@ -17,6 +45,8 @@ public class Alert {
             case "Moderate Wind Warning":
                 this.title = "Vents forts";
                 break;
+            case "Moderate Thunderstorm Warning":
+                this.title = "Risque modéré d'orages";
             default:
                 break;
         }
@@ -31,11 +61,15 @@ public class Alert {
         return severity;
     }
 
-    public Time getTime() {
-        return time;
+    public Time getStart() {
+        return start;
     }
 
     public Time getExpires() {
         return expires;
+    }
+
+    public boolean isMake() {
+        return isMake;
     }
 }
