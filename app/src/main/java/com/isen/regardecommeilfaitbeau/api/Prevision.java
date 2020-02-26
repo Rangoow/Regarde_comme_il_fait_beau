f997739761a842328a0c0f91e0e0be51f48c3840
Langue <louis-clement.langue@isen.yncrea.fr>
2020-02-26 22:08:44 +0100

    end Prevision

--- app/src/main/java/com/isen/regardecommeilfaitbeau/api/Prevision.java
+++ app/src/main/java/com/isen/regardecommeilfaitbeau/api/Prevision.java
@@ -1,4 +1,110 @@
 package com.isen.regardecommeilfaitbeau.api;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.isen.regardecommeilfaitbeau.api.Request.Request;
+import com.isen.regardecommeilfaitbeau.api.conversion.ConversionDaily;
+import com.isen.regardecommeilfaitbeau.api.conversion.ConversionHourly;
+import com.isen.regardecommeilfaitbeau.api.conversion.ConvertionAlerts;
+import com.isen.regardecommeilfaitbeau.api.meteo.Alert;
+import com.isen.regardecommeilfaitbeau.api.meteo.Currently;
+import com.isen.regardecommeilfaitbeau.api.meteo.MeteoDay;
+import com.isen.regardecommeilfaitbeau.exception.NumberHoursOfDayException;
+import com.isen.regardecommeilfaitbeau.typeData.Position;
+import com.isen.regardecommeilfaitbeau.typeData.Time;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Prevision {
+
+    private Position position;
+    private Time time;
+    private String timeZone;
+
+    private Request request;
+
+    private JSONObject currentlyJSON;
+    private JSONArray alertsJSON;
+    private JSONArray pastHours;
+    private JSONArray futurHours;
+    private JSONArray dailyJson;
+
+    private ConvertionAlerts convertionAlerts;
+    private ConversionHourly conversionHourly;
+    private ConversionDaily conversionDaily;
+
+    private Currently currently;
+    private ArrayList<Alert> alerts;
+    private ArrayList<MeteoDay> meteoDay;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Prevision(double latitude, double longitude) throws JSONException, NumberHoursOfDayException {
+        request = new Request(latitude, longitude);
+        makeObject();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Prevision(String nameCity) throws JSONException, NumberHoursOfDayException {
+        request = new Request(nameCity);
+        makeObject();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void makeObject() throws JSONException, NumberHoursOfDayException {
+        getRequestProperties();
+        convertionJSON();
+    }
+
+    private void getRequestProperties(){
+        position = request.getPosition();
+        time  = request.getActualTime();
+        timeZone = time.getZoneIdS();
+        currentlyJSON = request.getCurrently();
+        if(request.getAlerts() != null){
+            alertsJSON = request.getAlerts();
+        }
+        pastHours = request.getPastHours();
+        futurHours = request.getFuturHours();
+        dailyJson = request.getDaily();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void convertionJSON() throws JSONException, NumberHoursOfDayException {
+        currently = new Currently(currentlyJSON, timeZone);
+        if(alertsJSON != null){
+            convertionAlerts = new ConvertionAlerts(alertsJSON, time);
+            alerts = convertionAlerts.getAlerts();
+        }else {
+            alerts = new ArrayList<>();
+        }
+        conversionHourly = new ConversionHourly(futurHours, pastHours, time);
+        conversionDaily = new ConversionDaily(dailyJson, time, conversionHourly.getHoursPerDay());
+        meteoDay = conversionDaily.getDaily();
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public Currently getCurrently() {
+        return currently;
+    }
+
+    public ArrayList<Alert> getAlerts() {
+        return alerts;
+    }
+
+    public ArrayList<MeteoDay> getMeteoDay() {
+        return meteoDay;
+    }
 }
---
+++ app/src/test/java/com/isen/regardecommeilfaitbeau/PrevisionTest.java
@@ -0,0 +1,69 @@
+package com.isen.regardecommeilfaitbeau;
+
+import com.isen.regardecommeilfaitbeau.api.Prevision;
+import com.isen.regardecommeilfaitbeau.api.meteo.Alert;
+import com.isen.regardecommeilfaitbeau.api.meteo.Currently;
+import com.isen.regardecommeilfaitbeau.api.meteo.MeteoDay;
+import com.isen.regardecommeilfaitbeau.api.meteo.MeteoHour;
+import com.isen.regardecommeilfaitbeau.exception.NumberHoursOfDayException;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrevisionTest {
+
+    private Prevision previsionCoordonate;
+    private Prevision previsionCity;
+
+    @Before
+    public void makePrevision() throws JSONException, NumberHoursOfDayException {
+        previsionCoordonate = new Prevision(50.72301, 2.73896);
+        previsionCity = new Prevision("Bailleul");
+    }
+
+    @Test
+    public void currentlyTest(){
+        System.out.println("Test Currently");
+        Currently currentlyCoordonate = previsionCoordonate.getCurrently();
+        System.out.println(currentlyCoordonate.getSummary());
+        System.out.println(currentlyCoordonate.getIcon());
+        Currently currentlyCity = previsionCity.getCurrently();
+        System.out.println(currentlyCity.getSummary());
+        System.out.println(currentlyCity.getIcon());
+    }
+
+    @Test
+    public void alertsTest(){
+        System.out.println("Test Alerts");
+        ArrayList<Alert> alertsCoordonate = previsionCoordonate.getAlerts();
+        System.out.println(alertsCoordonate.size());
+        if(!alertsCoordonate.isEmpty()){
+            System.out.println(alertsCoordonate.get(0).getTitle());
+        }
+        ArrayList<Alert> alertsCity = previsionCity.getAlerts();
+        System.out.println(alertsCity.size());
+        if(!alertsCity.isEmpty()){
+            System.out.println(alertsCity.get(0).getTitle());
+        }
+    }
+
+    @Test
+    public void previsionTest(){
+        System.out.println("Test Previsions");
+        ArrayList<MeteoDay> dailyCoordonate = previsionCoordonate.getMeteoDay();
+        assertEquals(7, dailyCoordonate.size());
+        for(MeteoDay i:dailyCoordonate){
+            assertEquals(24, i.getHourByHour().size());
+        }
+        ArrayList<MeteoDay> dailyCity = previsionCity.getMeteoDay();
+        assertEquals(7, dailyCity.size());
+        for(MeteoDay i:dailyCity){
+            assertEquals(24, i.getHourByHour().size());
+        }
+    }
+}
